4f883f66e42f7af3e95cd7c6a11436d8f824aff4
Eugene <eugene.eu2015@yandex.ru>
2014-06-10 13:42:50 +0300

    initial commit

---
+++ PhotoShare/PhotoShare/PHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PHAppDelegate.h
+//  PhotoShare
+//
+//  Created by Евгений on 10.06.14.
+//  Copyright (c) 2014 Eugene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoShare/PhotoShare/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoShare
+//
+//  Created by Евгений on 10.06.14.
+//  Copyright (c) 2014 Eugene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PHAppDelegate class]));
+    }
+}
