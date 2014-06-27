6bbe7223232224df417a4b8fe1920eba05d57cea
Eugene <eugene.eu2015@yandex.ru>
2014-06-27 22:50:34 +0300

    Added ability to show time interval between photo date and now. Still writing category for UIViewController in order to share photos in different controllers. Problems: 1. Aforesaid category needs delegate and its method for mail_controller. 2. 'cell.selected=NO' doesn't work properly in PSStreamController (cells are selectable with grey background color) 3. If cell cannnot display an image ('like butterfly') - it takes some reusable cell with its image and display it.

---
+++ PhotoShare/PhotoShare/Categories/UIViewController+UIViewController_PSSharingDataComposer.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+UIViewController_PSSharingDataComposer.h
+//  PhotoShare
+//
+//  Created by Евгений on 27.06.14.
+//  Copyright (c) 2014 Eugene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (UIViewController_PSSharingDataComposer)
+
+@end
---
+++ PhotoShare/PhotoShare/Categories/UIViewController+UIViewController_PSSharingDataComposer.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+UIViewController_PSSharingDataComposer.m
+//  PhotoShare
+//
+//  Created by Евгений on 27.06.14.
+//  Copyright (c) 2014 Eugene. All rights reserved.
+//
+
+#import "UIViewController+UIViewController_PSSharingDataComposer.h"
+
+@implementation UIViewController (UIViewController_PSSharingDataComposer)
+
+@end
